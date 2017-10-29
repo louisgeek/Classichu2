6f72168d90d2161f16db76da90d6c2146eea8964
classichu <classichu@qq.com>
2017-10-29 21:11:09 +0800

    dialog login

--- app/build/generated/source/r/debug/com/jude/swipbackhelper/R.java
+++ app/build/generated/source/r/debug/com/jude/swipbackhelper/R.java
@@ -449,8 +449,8 @@ public final class R {
         public static final int abc_textfield_search_activated_mtrl_alpha = 0x7f02004f;
         public static final int abc_textfield_search_default_mtrl_alpha = 0x7f020050;
         public static final int abc_textfield_search_material = 0x7f020051;
-        public static final int notification_template_icon_bg = 0x7f02008d;
-        public static final int shadow_left = 0x7f020089;
+        public static final int notification_template_icon_bg = 0x7f02008e;
+        public static final int shadow_left = 0x7f02008a;
     }
     public static final class id {
         public static final int action0 = 0x7f0d00a1;
@@ -579,17 +579,17 @@ public final class R {
         public static final int abc_search_dropdown_item_icons_2line = 0x7f040018;
         public static final int abc_search_view = 0x7f040019;
         public static final int abc_select_dialog_material = 0x7f04001a;
-        public static final int notification_media_action = 0x7f040031;
-        public static final int notification_media_cancel_action = 0x7f040032;
-        public static final int notification_template_big_media = 0x7f040033;
-        public static final int notification_template_big_media_narrow = 0x7f040035;
-        public static final int notification_template_media = 0x7f04003a;
-        public static final int notification_template_part_chronometer = 0x7f04003c;
-        public static final int notification_template_part_time = 0x7f04003d;
-        public static final int select_dialog_item_material = 0x7f04003e;
-        public static final int select_dialog_multichoice_material = 0x7f04003f;
-        public static final int select_dialog_singlechoice_material = 0x7f040040;
-        public static final int support_simple_spinner_dropdown_item = 0x7f040041;
+        public static final int notification_media_action = 0x7f040032;
+        public static final int notification_media_cancel_action = 0x7f040033;
+        public static final int notification_template_big_media = 0x7f040034;
+        public static final int notification_template_big_media_narrow = 0x7f040036;
+        public static final int notification_template_media = 0x7f04003b;
+        public static final int notification_template_part_chronometer = 0x7f04003d;
+        public static final int notification_template_part_time = 0x7f04003e;
+        public static final int select_dialog_item_material = 0x7f04003f;
+        public static final int select_dialog_multichoice_material = 0x7f040040;
+        public static final int select_dialog_singlechoice_material = 0x7f040041;
+        public static final int support_simple_spinner_dropdown_item = 0x7f040042;
     }
     public static final class string {
         public static final int abc_action_bar_home_description = 0x7f070000;
--- app/src/main/java/com/classichu/classichu2/login/LoginFragment.java
+++ app/src/main/java/com/classichu/classichu2/login/LoginFragment.java
@@ -2,13 +2,16 @@ package com.classichu.classichu2.login;
 
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.classichu.classichu2.R;
 import com.classichu.classichu2.base.BaseFragment;
+import com.classichu.dialogview.ui.ClassicDialogFragment;
 import com.jakewharton.rxbinding2.view.RxView;
 
 import java.util.concurrent.TimeUnit;
@@ -51,11 +54,13 @@ public class LoginFragment extends BaseFragment {
         return R.layout.fragment_login;
     }
 
-    private Button id_btn_login = findById(R.id.id_btn_login);
-    private TextView id_tv_root = findById(R.id.id_tv_root);
+    private Button id_btn_login;
+    private TextView id_tv_admin;
 
     @Override
     protected void initView(View rootLayout, Bundle savedInstanceState) {
+        id_btn_login = findById(R.id.id_btn_login);
+        id_tv_admin = findById(R.id.id_tv_admin);
 
 
         RxView.clicks(id_btn_login)
@@ -64,21 +69,47 @@ public class LoginFragment extends BaseFragment {
                 .subscribe(new Consumer<Object>() {
                     @Override
                     public void accept(@NonNull Object o) throws Exception {
-                        RxView.enabled(id_btn_login).accept(true);
-
-
+                        RxView.enabled(id_btn_login).accept(false);
+                        Snackbar.make(mRootLayout,"login",Snackbar.LENGTH_SHORT).show();
                     }
                 });
 
-        RxView.clicks(id_tv_root)
+
+        RxView.clicks(id_tv_admin)
                 .subscribe(new Consumer<Object>() {
                     @Override
                     public void accept(@NonNull Object o) throws Exception {
 
+                        showLoginAdminDialog();
                     }
                 });
 
 
+    }
+
+    private ClassicDialogFragment classicDialogFragment;
+    private void showLoginAdminDialog() {
+        //
+        View view = LayoutInflater.from(mActivity).inflate(R.layout.content_login, null);
+        final View id_btn_login_admin = view.findViewById(R.id.id_btn_login);
+        RxView.clicks(id_btn_login_admin)
+                .throttleFirst(3, TimeUnit.SECONDS)
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(@NonNull Object o) throws Exception {
+                        RxView.enabled(id_btn_login_admin).accept(false);
+                        Snackbar.make(mRootLayout,"admin login",Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+
+        if (classicDialogFragment != null) {
+            classicDialogFragment.dismissAllowingStateLoss();
+            classicDialogFragment = null;
+        }
+        classicDialogFragment = new ClassicDialogFragment.Builder(mActivity).setCancelable(true).setTitle(R.string.login_admin_login)
+                .setContentView(view).build();
+        classicDialogFragment.show(getChildFragmentManager(), "showLoginAdminDialog");
     }
 
 }
